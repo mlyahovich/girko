9325ca7f2bbff17be846db861cc99566dcd7e843
nasa <m.lyahovich@internetdevels.com>
2017-03-11 16:11:01 +0200

    [G] Add libraries.

---
+++ themes/custom/girko/js/script.js
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+(function ($, Drupal) {
+    /**
+     * Initialization
+     */
+    Drupal.behaviors.colorbox_swipe = {
+        /**
+         * Run Drupal module JS initialization.
+         *
+         * @param context
+         * @param settings
+         */
+        attach: function(context, settings) {
+            $(document).ready(function() {
+                // Global context!
+                $("#colorbox *", context).bind(
+                    'swipeleft',
+                    function(e) {
+                        // Disable selection.
+                        $(this).disableSelection();
+                        $.colorbox.next();
+                    }).bind(
+                    'swiperight',
+                    function(e) {
+                        // Disable selection.
+                        $(this).disableSelection();
+                        $.colorbox.prev();
+                    });
+            });
+        }
+    }
+})(jQuery, Drupal);
